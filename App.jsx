7503d17817565840800d72a846fdbbe4eebf1714
Shikhar Verma <158575502+shikhar-verma-1@users.noreply.github.com>
2024-02-09 18:20:37 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Navbar'
+import Main from './components/Main'
+
+export default function App(){
+    return(
+        <div>
+            <Header />
+            <Main />
+        </div>
+    )
+}
